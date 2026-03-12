fb170138f4d22c63b36a9d6ca632fca188062261
Gavin Morrice <gavin@gavinmorrice.com>
2026-03-12 08:54:46 +0000

    Add extra tests for user addresses

--- test/controllers/users/addresses_controller_test.rb
+++ test/controllers/users/addresses_controller_test.rb
@@ -2,9 +2,32 @@ require "test_helper"
 
 class Users::AddressesControllerTest < ActionDispatch::IntegrationTest
   test "should get best correspondence address for User" do
-    user = FactoryBot.create(:user, shipping_address_line_1: "Shipping address 1")
+    user = create(:user,
+                             # Return the shipping address
+                             shipping_address_line_1: "Shipping address 1",
+                             shipping_address_postcode: "AB12 3CD",
+                             # Do not return the billing address
+                             billing_address_line_1: "Billing address 1",
+                             billing_address_postcode: "AB45 5XY")
+
     get "/users/#{user.id}/addresses/correspondence"
+
     assert_response :success
     assert_includes response.body, "Shipping address 1"
+  end
+
+  test "should fall back on business address for User if shipping not usable" do
+    user = create(:user,
+                  # Return the shipping address
+                  shipping_address_line_1: "Shipping address 1",
+                  shipping_address_postcode: "", #intentionally blank
+                  # Do not return the billing address
+                  billing_address_line_1: "Billing address 1",
+                  billing_address_postcode: "AB45 5XY")
+
+    get "/users/#{user.id}/addresses/correspondence"
+
+    assert_response :success
+    assert_includes response.body, "Billing address 1"
   end
 end
