2184b6cb19b3331579c4fbc81503d6cab3ebcb82
Gavin Morrice <gavin@gavinmorrice.com>
2026-03-13 16:18:28 +0000

    Make country explicit in the User factory

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -7,13 +7,13 @@ FactoryBot.define do
     shipping_address_line_2 { Faker::Address.secondary_address }
     shipping_address_postcode { Faker::Address.postcode }
     shipping_address_county { Faker::Address.state }
-    shipping_address_country { Faker::Address.country }
+    shipping_address_country  { "United Kingdom" }
     shipping_address_town { Faker::Address.city }
     billing_address_line_1 { Faker::Address.street_name }
     billing_address_line_2 { nil }
     billing_address_postcode { Faker::Address.postcode }
     billing_address_county { Faker::Address.state }
-    billing_address_country { Faker::Address.country }
+    billing_address_country { "United Kingdom" }
     billing_address_town { Faker::Address.city }
   end
 end
